b923ef2cea671c40ca83b655fefcad7b3619a33d
Brunno Cabral <brunnocabral2020@gmail.com>
2023-06-01 11:39:50 -0300

    Fix the search bar
    fix the search bar when its value is empty

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,7 +1,7 @@
 // import styled from "styled-components";
 import styled from "styled-components";
 import Input from "../Input/input";
-import { dadosLivros } from "./testeBrunno";
+import { books } from "./testeBrunno";
 import { useState } from "react";
 
 const SearchContainer = styled.section`
@@ -27,21 +27,21 @@ const Subtitle = styled.h2`
 `;
 
 const Result = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 20px;
-    cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
 
-    p {
-        width: 200px;
-    }
-    img {
-        width: 100px;
-    }
-    &:hover {
-        border: 1px solid white;
-    }
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
 `;
 
 /**
@@ -60,17 +60,21 @@ function Search() {
         onBlur={(event) => {
           const typedText = event.target.value;
 
-          const searchResult = dadosLivros.filter((livro) =>
-            livro.nome.includes(typedText)
-          );
+          const searchResult = books.filter((livro) => {
+            if (typedText !== "" && livro.nome.includes(typedText)) {
+              return true;
+            } else {
+              return false;
+            }
+          });
 
           setResearchedBooks(searchResult);
         }}
       />
-      {researchedBooks.map((livro) => (
+      {researchedBooks.map((livros) => (
         <Result>
-          <img src={livro.src} alt="capa do livro" />
-          <p>{livro.nome}</p>
+          <img src={livros.src} alt="capa do livro" />
+          <p>{livros.nome}</p>
         </Result>
       ))}
     </SearchContainer>
